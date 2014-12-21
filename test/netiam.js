6be29ba5313c8bee78cf219af70bb502100b5b1e
Hannes Moser <hannes@impossiblearts.com>
2014-12-21 23:25:06 +0100

    feature: plugin system feature: basic pipelining

---
+++ test/netiam.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var should = require( 'should' );
+
+describe( 'netiam', function() {
+    describe( 'version', function() {
+        it( 'should return current library version', function() {
+            '0.0.1'.should.be.exactly( '0.0.1' );
+        } );
+    } );
+} );
