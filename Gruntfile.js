1bb5126808bdbd9f6fb9319f6d47df47783aab13
Hannes Moser <hannes@impossiblearts.com>
2014-12-23 23:36:11 +0100

    feature: added automatic version bump and changelog generation

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,34 @@ module.exports = function( grunt ) {
     require( 'load-grunt-tasks' )( grunt );
 
     grunt.initConfig( {
-        pkg:    grunt.file.readJSON( 'package.json' ),
+        pkg: grunt.file.readJSON( 'package.json' ),
 
-        // JSHint
+        changelog: {
+            sample: {
+                options: {}
+            }
+        },
+
+        bump: {
+            options: {
+                files:              ['package.json'],
+                updateConfigs:      [],
+                commit:             true,
+                commitMessage:      'Release v%VERSION%',
+                commitFiles:        ['package.json'],
+                createTag:          true,
+                tagName:            'v%VERSION%',
+                tagMessage:         'Version %VERSION%',
+                push:               true,
+                pushTo:             'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace:      false
+            }
+        },
+
         jshint: {
             options: {jshintrc: true},
-            all:    [
+            all:     [
                 './Gruntfile.js',
                 './lib/**/*.js'
             ]
