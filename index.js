52710b0fab02048281a26fdcf7a0c474a1962015
Hannes Moser <hannes@impossiblearts.com>
2015-05-16 21:33:26 +0200

    feat: expose error

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const error = require('./lib/rest/error')
 const lib = require('./lib/netiam')
 const middleware = require('./lib/middleware')
 const plugins = require('./lib/plugins')
 const schemaPlugins = require('./lib/rest/schema/plugins')
 
+lib.error = error
 lib.middleware = middleware
 lib.plugins = plugins
 lib.schema = {
