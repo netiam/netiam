01127901a7a2e2c6b814a68d5a8563d0fecaf209
Hannes Moser <hannes@impossiblearts.com>
2015-05-21 18:30:51 +0200

    refactor: collections are models(as it is with mongoose) feat: export schmes provided by library

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const error = require('./lib/rest/error')
 const lib = require('./lib/netiam')
 const middleware = require('./lib/middleware')
 const plugins = require('./lib/plugins')
+const Role = require('./lib/rest/schema/role')
 const schemaPlugins = require('./lib/rest/schema/plugins')
 
 lib.error = error
 lib.middleware = middleware
 lib.plugins = plugins
 lib.schema = {
+  Role: Role,
   plugins: schemaPlugins
 }
 
--- src/rest/roles.js
+++ src/rest/roles.js
@@ -1,5 +1,5 @@
 import _ from 'lodash'
-import Role from './collections/role'
+import Role from 'models/role'
 
 const documents = [
   {
