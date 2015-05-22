8b8c3a30d5d127efc5e0f34ffcf7dbe921df1524
Hannes Moser <hannes@impossiblearts.com>
2015-05-22 13:18:30 +0200

    feat: export ACL loaders

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const error = require('./lib/rest/error')
 const lib = require('./lib/netiam')
 const middleware = require('./lib/middleware')
 const plugins = require('./lib/plugins')
-const Role = require('./lib/rest/schema/role')
+const RoleSchema = require('./lib/rest/schema/role')
 const schemaPlugins = require('./lib/rest/schema/plugins')
+const aclLoader = require('./lib/acl/loader')
 
+lib.acl = {
+  loader: aclLoader
+}
 lib.error = error
 lib.middleware = middleware
 lib.plugins = plugins
 lib.schema = {
-  Role: Role,
+  Role: RoleSchema,
   plugins: schemaPlugins
 }
 
