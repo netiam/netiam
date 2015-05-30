6a5322c1d8254fe06bac83fcb77e562565db8e9d
Hannes Moser <hannes@impossiblearts.com>
2015-05-30 19:08:48 +0200

    fix: add missing fiels

--- src/rest/schema/plugins/created.js
+++ src/rest/schema/plugins/created.js
@@ -1,5 +1,7 @@
 export default function created(schema) {
 
+  schema.add({created: Date})
+
   schema.pre('save', function(next) {
     if (!this.created) {
       this.created = new Date()
--- src/rest/schema/plugins/modified.js
+++ src/rest/schema/plugins/modified.js
@@ -1,5 +1,7 @@
 export default function modified(schema) {
 
+  schema.add({modified: Date})
+
   schema.pre('save', function(next) {
     this.modified = new Date()
 
