9b11b2fe09eeb3757d8578d257fe3a5077731cb9
Hannes Moser <hannes@impossiblearts.com>
2015-05-31 02:25:57 +0200

    feat: remove login plugin
    BREAKING CHANGE: remove login, use auth instead

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -5,7 +5,6 @@ import data from './data'
 import graph from './graph'
 import json from './json'
 import jsonapi from './jsonapi'
-import login from './login'
 import map from './map'
 import profile from './profile'
 import render from './render'
@@ -20,7 +19,6 @@ export default {
   graph,
   json,
   jsonapi,
-  login,
   map,
   profile,
   render,
