c198fd034975d39e8d8946749de3f998efabd69f
Hannes Moser <hannes@impossiblearts.com>
2015-07-27 15:56:56 +0200

    fix: resource creation returns bad request if document exists

--- src/rest/resource.js
+++ src/rest/resource.js
@@ -306,6 +306,11 @@ export default function resource(spec) {
         .create(req.body, (err, documents) => {
           if (err) {
             debug(err)
+
+            if (err.code === 11000) {
+              return reject(error.badRequest(err.message))
+            }
+
             return reject(error.internalServerError(err.message))
           }
           if (!documents) {
