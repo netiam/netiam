cca7ca8bc905390a101507cf15a4ba2be616e283
Hannes Moser <hannes@impossiblearts.com>
2015-08-21 17:21:26 +0200

    fix: enable virtuals by default

--- src/rest/schema/plugins/type.js
+++ src/rest/schema/plugins/type.js
@@ -1,5 +1,8 @@
 export default function type(schema, spec) {
 
+  schema.set('toObject', {virtuals: true})
+  schema.set('toJSON', {virtuals: true})
+
   schema
     .virtual('type')
     .get(function() {
