bce02be903ba5a5e47a636f25547d59e72128544
Hannes Moser <hannes@impossiblearts.com>
2015-08-24 21:53:50 +0200

    test: add json plugin test

--- test/plugins/index.js
+++
@@ -1,9 +0,0 @@
-import client from './client'
-import jsonapi from './jsonapi'
-import transform from './transform'
-
-describe('Plugins', function() {
-  client()
-  jsonapi()
-  transform()
-})
---
+++ test/plugins/json.js
@@ -0,0 +1,200 @@
+import _ from 'lodash'
+import request from 'supertest'
+import User from './../models/user'
+import fixtures from '../fixtures'
+import roles from '../../src/rest/roles'
+import Role from '../../src/rest/models/role'
+import db from './../utils/db.test.js'
+import api from '../../src/netiam'
+
+export default function() {
+
+  const userFixture = require('./../fixtures/user.json')
+  const app = require('./../utils/app.test.js')()
+  let userId
+
+  before(function(done) {
+    app.post(
+      '/users',
+      api()
+        .rest({collection: User})
+        .map.res({_id: 'id'})
+        .json({collection: User})
+    )
+
+    app.get(
+      '/users-plain',
+      api()
+        .rest({
+          collection: User,
+          itemsPerPage: 1
+        })
+        .map.res({_id: 'id'})
+        .json()
+    )
+
+    app.get(
+      '/users',
+      api()
+        .rest({
+          collection: User,
+          itemsPerPage: 1
+        })
+        .map.res({_id: 'id'})
+        .json({collection: User})
+    )
+
+    app.get(
+      '/users-plain/:id',
+      api()
+        .rest({collection: User})
+        .map.res({_id: 'id'})
+        .json()
+    )
+
+    app.get(
+      '/users/:id',
+      api()
+        .rest({collection: User})
+        .map.res({_id: 'id'})
+        .json({collection: User})
+    )
+
+    app.put(
+      '/users/:id',
+      api()
+        .rest({collection: User})
+        .map.res({_id: 'id'})
+        .json({collection: User})
+    )
+
+    fixtures(function(err) {
+      if (err) {
+        return done(err)
+      }
+
+      Role.find({}, function(err, docs) {
+        if (err) {
+          return done(err)
+        }
+
+        roles.set(docs)
+        done()
+      })
+    })
+  })
+
+  after(done => {
+    db.connection.db.dropDatabase(err => {
+      if (err) {
+        return done(err)
+      }
+      done()
+    })
+  })
+
+  describe('json', function() {
+    it('should create a user', function(done) {
+      request(app)
+        .post('/users')
+        .send(userFixture)
+        .set('Accept', 'application/json')
+        .expect(201)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) {
+            return done(err)
+          }
+
+          res.body.should.have.property('user')
+          res.body.user.should.be.an.Object()
+
+          userId = res.body.user.id
+
+          done()
+        })
+    })
+
+    it('should get users - plain', function(done) {
+      request(app)
+        .get('/users-plain')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) {
+            return done(err)
+          }
+
+          res.body.should.be.an.Array()
+          res.body.should.have.length(1)
+
+          done()
+        })
+    })
+
+    it('should get users - typed', function(done) {
+      request(app)
+        .get('/users')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) {
+            return done(err)
+          }
+
+          res.body.should.be.an.Object()
+          res.body.should.have.property('user')
+          res.body.user.should.be.an.Array()
+          res.body.user.should.have.length(1)
+
+          done()
+        })
+    })
+
+    it('should get a user - plain', function(done) {
+      request(app)
+        .get('/users-plain/' + userId)
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) {
+            return done(err)
+          }
+
+          res.body.should.be.an.Object()
+          res.body.should.have.property('id')
+
+          done()
+        })
+    })
+
+    it('should modify a user', function(done) {
+      let modifiedUser = _.clone(userFixture)
+      modifiedUser.name = 'modified name'
+
+      request(app)
+        .put('/users/' + userId)
+        .send(modifiedUser)
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          if (err) {
+            return done(err)
+          }
+
+          res.body.should.be.an.Object()
+          res.body.should.have.property('user')
+          res.body.user.should.be.an.Object()
+          res.body.user.should.have.property('id')
+
+          done()
+        })
+    })
+
+  })
+
+}
