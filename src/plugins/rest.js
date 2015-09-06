c96ec3c7863dbe79dceeb98c7f234ff9f7f9566a
Hannes Moser <hannes@impossiblearts.com>
2015-09-06 18:52:29 +0200

    feat: handle methods according to relationship type

--- src/plugins/rest.js
+++ src/plugins/rest.js
@@ -1,15 +1,31 @@
+import * as errors from 'netiam-errors'
 import resourceRest from '../rest/resource'
+import {ONE_TO_MANY, MANY_TO_ONE} from '../rest/relationships'
 
 export default function rest(spec) {
   const resource = resourceRest(spec)
-  let {idParam} = spec
-
-  idParam = idParam || 'id'
+  const {relationship} = spec
 
   return async function(req, res) {
     const method = req.method
+    let isList = false
+    let idParam = spec.idParam || 'id'
 
-    if (method === 'GET' && !req.params[idParam]) {
+    if (!req.params[idParam]) {
+      isList = true
+    }
+
+    if (relationship && relationship.type === ONE_TO_MANY) {
+      if (req.idParam === relationship.idParam) {
+        throw errors.internalServerError(
+          'The relationship "idParam" must not match the base "idParam"')
+      }
+      if (!req.params[relationship.idParam]) {
+        isList = true
+      }
+    }
+
+    if (method === 'GET' && isList) {
       res.body = await resource.list(req, res)
       return res.status(200)
     }
@@ -24,7 +40,12 @@ export default function rest(spec) {
       return res.status(200)
     }
 
-    if (method === 'PUT' || method === 'PATCH') {
+    if (method === 'PATCH') {
+      res.body = await resource.update(req, res)
+      return res.status(200)
+    }
+
+    if (method === 'PUT') {
       res.body = await resource.update(req, res)
       return res.status(200)
     }
