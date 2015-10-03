537777d984abc1e07caa67283ae17d114c4a68f0
Hannes Moser <hannes@impossiblearts.com>
2015-10-03 18:22:02 +0200

    refactor: extract relationship methods

--- src/rest/methods/create.js
+++ src/rest/methods/create.js
@@ -1,6 +1,10 @@
 import _ from 'lodash'
 import dbg from 'debug'
-import * as errors from 'netiam-errors'
+import {
+  NotFound,
+  InternalServerError,
+  Codes
+} from 'netiam-errors'
 import {normalize} from '../query'
 import {ONE_TO_MANY, MANY_TO_ONE} from '../relationships'
 
@@ -8,125 +12,86 @@ const debug = dbg('netiam:rest:resource:create')
 
 function create(collection, document, queryNormalized) {
   if (!document) {
-    return Promise.reject(errors.internalServerError(
-      'Document could not be created',
-      [errors.Codes.E3000]))
+    throw new InternalServerError(Codes.E1000, 'Document could not be created')
   }
+
   if (queryNormalized.expand.length > 0) {
     return collection
       .find(document)
       .populate(queryNormalized.expand)
-  } else {
-    return Promise.resolve(document)
   }
+
+  return document
+}
+
+function oneToMany(spec) {
+  let baseDocument
+
+  return spec.collection
+    .findOne({[spec.idField]: spec.req.params[spec.idParam]})
+    .then(document => {
+      if (!document) {
+        throw new NotFound(Codes.E1000, 'Base document does not exist')
+      }
+
+      baseDocument = document
+      return spec.relationship.Model.create(req.body)
+    })
+    .then(document => {
+      return create(document, spec.queryNormalized)
+    })
+    .then(document => {
+      const relationshipField = spec.relationship.field
+      const relationshipIdField = spec.relationship.idField
+
+      if (!document) {
+        throw new InternalServerError(Codes.E1000, 'Cannot created document')
+      }
+
+      if (!_.isArray(baseDocument[relationshipField])) {
+        baseDocument[relationshipField] = []
+      }
+      baseDocument[relationshipField].push(document[relationshipIdField])
+
+      return baseDocument
+        .save()
+        .then(() => {
+          return document
+        })
+    })
+}
+
+function manyToOne(spec) {
+  const relationshipIdField = spec.relationship.idField
+  const relationshipId = req.params[spec.relationship.idParam]
+
+  return spec.relationship.Model
+    .findOne({[relationshipIdField]: relationshipId})
+    .then(document => {
+      if (!document) {
+        throw new NotFound(Codes.E1000, 'Base document does not exist')
+      }
+
+      const body = Object.assign(
+        {},
+        req.body,
+        {[spec.relationship.field]: document[relationshipIdField]}
+      )
+      return spec.collection.create(body)
+    })
+    .then(document => {
+      return create(document, spec.queryNormalized)
+    })
 }
 
 function handleRelationship(spec) {
-  const {req} = spec
-
   if (ONE_TO_MANY === spec.relationship.type) {
-    return new Promise((resolve, reject) => {
-      spec.collection
-        .findOne({[spec.idField]: spec.req.params[spec.idParam]})
-        .exec((err, document) => {
-          if (err) {
-            debug(err)
-            return reject(
-              errors.internalServerError(err, [errors.Codes.E3000]))
-          }
-
-          if (!document) {
-            return reject(errors.notFound(
-              'Base document does not exist', [errors.Codes.E3000]))
-          }
-
-          spec.relationship.Model
-            .create(req.body, (err, doc) => {
-              const relationshipField = spec.relationship.field
-              const relationshipIdField = spec.relationship.idField
-
-              create(doc, spec.queryNormalized)
-                .then(doc => {
-                  if (!_.isArray(document[relationshipField])) {
-                    document[relationshipField] = []
-                  }
-                  document[relationshipField].push(doc[relationshipIdField])
-                  document.save(err => {
-                    if (err) {
-                      return reject(err)
-                    }
-
-                    resolve(doc)
-                  })
-                })
-                .catch(err => {
-                  return reject(error(err))
-                })
-            })
-        })
-    })
+    return oneToMany(spec)
   }
 
   if (MANY_TO_ONE === spec.relationship.type) {
-    const relationshipIdField = spec.relationship.idField
-    const relationshipId = req.params[spec.relationship.idParam]
-
-    return new Promise((resolve, reject) => {
-      spec.relationship.Model
-        .findOne({[relationshipIdField]: relationshipId})
-        .exec((err, document) => {
-          if (err) {
-            debug(err)
-            return reject(
-              errors.internalServerError(err, [errors.Codes.E3000])
-            )
-          }
-
-          if (!document) {
-            return reject(errors.notFound(
-              'Base document does not exist', [errors.Codes.E3000]))
-          }
-
-          const body = Object.assign(
-            {},
-            req.body,
-            {[spec.relationship.field]: document[relationshipIdField]}
-          )
-
-          spec.collection.create(body, (err, doc) => {
-            create(doc, spec.queryNormalized)
-              .then(resolve)
-              .catch(err => {
-                return reject(error(err))
-              })
-          })
-        })
-    })
+    return manyToOne(spec)
   }
-}
-
-function error(err) {
-  debug(err)
-  if (err.code === 11000) {
-    return errors.badRequest(err, [errors.Codes.E1001])
-  }
-
-  if (err.name === 'ValidationError') {
-    const errList = []
-
-    _.forEach(err.errors, error => {
-      const modError = Object.assign({
-        path: error.path,
-        value: error.value
-      }, errors.Codes.E3002)
-
-      errList.push(modError)
-    })
-
-    return errors.badRequest(err, errList)
-  }
-
-  return errors.internalServerError(err, [errors.Codes.E3000])
 }
 
 /**
@@ -161,8 +126,5 @@ export default function(spec) {
     .create(req.body)
     .then(document => {
       return create(collection, document, queryNormalized)
-    })
-    .catch(err => {
-      throw error(err)
     })
 }
