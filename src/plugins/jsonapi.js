4dea840973e56644d1c91b84287b59b0db29a8b9
Hannes Moser <hannes@impossiblearts.com>
2015-10-19 17:43:11 +0200

    chore: adapt jsonapi plugin to new library structure

--- src/plugins/jsonapi.js
+++ src/plugins/jsonapi.js
@@ -2,108 +2,74 @@ import {
   BadRequest,
   InternalServerError,
   NotImplemented,
+  NotFound,
   Codes
 } from 'netiam-errors'
-import {ONE_TO_MANY, MANY_TO_ONE} from '../rest/resource'
+import {
+  ONE_TO_MANY,
+  MANY_TO_ONE
+} from '../rest/relationships'
 import jsonapi from '../rest/jsonapi'
 import {normalize, params} from '../rest/query'
 import filter from '../rest/odata/filter'
+import {getCollectionByIdentity} from '../db'
 import dbg from 'debug'
 
 const debug = dbg('netiam:plugins:jsonapi')
 
 function request() {
-  throw new NotImplemnted(Codes.E1000, 'jsonapi#request is not implemented')
+  return Promise.reject(
+    new NotImplemnted(Codes.E1000, 'jsonapi#request is not implemented'))
 }
 
 function response(spec) {
-  const {collection} = spec
+  const collection = getCollectionByIdentity(spec.collection)
   const {relationshipField} = spec
-  const {relationshipCollection} = spec
+  const relationshipCollection = getCollectionByIdentity(spec.relationshipCollection)
   const {map} = spec
   const {idField = 'id'} = spec
-  const {relationship = ONE_TO_MANY} = spec
+  const {relationship} = spec
 
   function numTotalDocuments(req, query) {
-    return new Promise((resolve, reject) => {
+    // filter
+    let f = filter(query.filter)
 
-      // filter
-      let f = filter(query.filter)
+    // handle relationships
+    if (relationship === MANY_TO_ONE &&
+      relationshipField &&
+      relationshipCollection &&
+      map) {
 
-      // handle relationships
-      if (relationship === MANY_TO_ONE &&
-        relationshipField &&
-        relationshipCollection &&
-        map) {
+      const key = Object.keys(map)[0]
+      const val = req.params[map[key].substr(1)]
 
-        const key = Object.keys(map)[0]
-        const val = req.params[map[key].substr(1)]
-
-        collection
-          .findOne({[key]: val})
-          .select(relationshipField)
-          .exec((err, doc) => {
-            if (err) {
-              debug(err)
-              return reject(err)
-            }
-
-            if (!doc) {
-              return resolve([])
-            }
-
-            // query
-            let q
-            try {
-              q = relationshipCollection.count(f.toObject())
-            } catch (err) {
-              debug(err)
-              reject(new BadRequest(Codes.E1000, err.message))
-            }
-
-            // select only related
-            q = q.where(idField).in(doc[relationshipField])
-
-            // execute
-            q.exec((err, count) => {
-              if (err) {
-                debug(err)
-                return reject(new InternalServerError(Codes.E1000, err.message))
-              }
-
-              resolve(count)
-            })
-          })
-
-        return
-      }
-
-      if (relationship === ONE_TO_MANY) {
-        f = f.where(params({
-          req,
-          map
-        }))
-
-        // query
-        let q
-        try {
-          q = collection.count(f.toObject())
-        } catch (err) {
-          debug(err)
-          reject(new BadRequest(Codes.E1000, err.message))
-        }
-
-        // execute
-        q.exec((err, count) => {
-          if (err) {
-            debug(err)
-            return reject(new InternalServerError(Codes.E1000, err.message))
+      return collection
+        .findOne({[key]: val})
+        .then(doc => {
+          if (!doc) {
+            throw new NotFound(Codes.E1000, 'Base document not found')
           }
 
-          resolve(count)
+          // query
+          return relationshipCollection
+            .count(f.toObject())
+            .where({[idField]: doc[relationshipField]})
         })
-      }
-    })
+    }
+
+    if (relationship === ONE_TO_MANY) {
+      f = f.where(params({
+        req,
+        map
+      }))
+
+      // query
+      return collection.count(f.toObject())
+    }
+
+    // query
+    // FIXME ONE_TO_MANY and no relationship is different
+    return collection.count()
   }
 
   return (req, res) => {
@@ -113,29 +79,23 @@ function response(spec) {
         .end()
     }
 
-    return new Promise((resolve, reject) => {
-      const queryNormalized = normalize({
-        req,
-        idField
-      })
-
-      numTotalDocuments(req, queryNormalized)
-        .then(count => {
-          res
-            .set('Content-Type', 'application/vnd.api+json')
-            .json(jsonapi.transform({
-              req,
-              res,
-              count,
-              itemsPerPage: queryNormalized.itemsPerPage,
-              collection
-            }))
-        })
-        .catch(err => {
-          debug(err)
-          reject(err)
-        })
+    const queryNormalized = normalize({
+      req,
+      idField
     })
+
+    return numTotalDocuments(req, queryNormalized)
+      .then(count => {
+        res
+          .set('Content-Type', 'application/vnd.api+json')
+          .json(jsonapi.transform({
+            req,
+            res,
+            count,
+            itemsPerPage: queryNormalized.itemsPerPage,
+            collection
+          }))
+      })
   }
 
 }
