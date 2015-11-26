36823367b16cf4c6bb308e9201562ea7a5e6cc9f
Hannes Moser <hannes@impossiblearts.com>
2015-11-26 16:30:38 +0100

    test: use arrow functions

--- test/cache/file.js
+++ test/cache/file.js
@@ -3,14 +3,14 @@ import fileCache from '../../src/cache/file'
 export default function() {
   const cache = fileCache({path: '.tmp/cache'})
 
-  it('should create cache entry', function(done) {
+  it('should create cache entry', done => {
     cache
       .save('test_id', 'Hello, World!')
       .then(done)
       .catch(done)
   })
 
-  it('should verify that cache entry exists', function(done) {
+  it('should verify that cache entry exists', done => {
     cache
       .has('test_id')
       .then(val => {
@@ -20,20 +20,20 @@ export default function() {
       .catch(done)
   })
 
-  it('should verify that cache entry does not exist', function(done) {
+  it('should verify that cache entry does not exist', done => {
     cache
       .has('does_not_exist')
-      .then(function(val) {
+      .then(val => {
         val.should.eql(false)
         done()
       })
       .catch(done)
   })
 
-  it('should load a cache entry', function(done) {
+  it('should load a cache entry', done => {
     cache
       .load('test_id')
-      .then(function(val) {
+      .then(val => {
         val.should.be.eql('Hello, World!')
         done()
       })
